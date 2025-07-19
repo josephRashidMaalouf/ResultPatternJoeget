6749e389e0d2ddb8c739f7b6ed65377a39f3458c
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-07-19 13:50:16 +0200

    .

--- ResultPatternJoeget/Results/ErrorResult.cs
+++ ResultPatternJoeget/Results/ErrorResult.cs
@@ -5,7 +5,6 @@ namespace ResultPatternJoeget.Results;
 public class ErrorResult : Result
 {
     public List<Error> Errors { get; } = [];
-
     public ErrorResult(params Error[] error)
     {
         Success = false;
